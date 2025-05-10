562bbfd8c9ccaded08807f44acd59f8be1587504
Wildboar14 <92429185+OwenRThomson@users.noreply.github.com>
2025-05-10 10:42:26 +0100

    feat(types): :label: Create initial types
    I like to start of thinking about a project by the types involved - helps me think about what needs to be done

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,1 +1,18 @@
-export type XorO = 'X' | 'O'
+export type XorO = "X" | "O";
+
+export type Board = (XorO | null)[][];
+/* I prefer NULL to undefined for the board empty state */
+
+export type Player = {
+  id: string;
+  name: string;
+  symbol: XorO;
+};
+
+// My initial idea for a game state that may be rewritten later
+export type GameState = {
+  board: Board;
+  currentPlayer: Player;
+  winner: Player | null;
+  isDraw: boolean;
+};
